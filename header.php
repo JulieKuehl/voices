7f47cd06293a8de91f714e477887769404ca2408
Julie Kuehl <julie@juliekuehl.com>
2015-07-20 13:40:00 -0500

    incorporate header, incl. styles

--- header.php
+++ header.php
@@ -15,23 +15,42 @@
 <link rel="profile" href="http://gmpg.org/xfn/11">
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 
+<!-- Google Fonts -->
+<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,700' rel='stylesheet' type='text/css'>
+<!-- Stylesheet -->
+<link rel="stylesheet" href="/wp-content/themes/areavoices/style.css">
+<link rel="stylesheet" href="/wp-content/themes/areavoices/homemade_style.css">
+
 <?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
-<div id="page" class="hfeed site">
+<div id="page" class="hfeed site container">
 	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'areavoices' ); ?></a>
 
 	<header id="masthead" class="site-header" role="banner">
-		<div class="site-branding">
-			<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-			<p class="site-description"><?php bloginfo( 'description' ); ?></p>
-		</div><!-- .site-branding -->
 
-		<nav id="site-navigation" class="main-navigation" role="navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'areavoices' ); ?></button>
-			<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
-		</nav><!-- #site-navigation -->
+		<div class="super-header">
+			<div class="social-links clearfix">
+				<a href="http://www.facebook.com" class="ir facebook" target="_blank">Facebook</a>
+				<a href="http://www.twitter.com" class="ir twitter" target="_blank">Twitter</a>
+				<a href="http://www.instagram.com" class="ir instagram" target="_blank">Instagram</a>
+			</div><!-- .social-links -->
+		</div><!-- .super-header -->
+
+		<div class="site-branding site-header">
+			<div class="container">
+				<div class="site-info">
+					<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+					<p class="site-description"><?php bloginfo( 'description' ); ?></p>
+				</div><!-- .site-info -->
+			</div><!-- .container -->
+		</div><!-- .site-branding .site-header-->
+
+<!--		<nav id="site-navigation" class="main-navigation" role="navigation">-->
+<!--			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false">--><?php //esc_html_e( 'Primary Menu', 'areavoices' ); ?><!--</button>-->
+<!--			--><?php //wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
+<!--		</nav><!-- #site-navigation -->
 	</header><!-- #masthead -->
 
-	<div id="content" class="site-content">
+<!--	<div id="content" class="site-content">-->
--- index.php
+++ index.php
@@ -1,96 +1,48 @@
-<!DOCTYPE html>
-<html lang="en-US">
-  <head>
-    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-    <title>Area Voices | Site Title</title>
-    <meta name="description" content="The Official Website of funk/reggae band The Young Funk">
-    
-    <!-- Google Fonts -->
-    <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,700' rel='stylesheet' type='text/css'>
-    <!-- Stylesheet -->
-    <link rel="stylesheet" href="/wp-content/themes/areavoices/style.css">
-  </head>
+<?php
+/**
+ * The main template file.
+ *
+ * This is the most generic template file in a WordPress theme
+ * and one of the two required files for a theme (the other being style.css).
+ * It is used to display a page when nothing more specific matches a query.
+ * E.g., it puts together the home page when no home.php file exists.
+ * Learn more: http://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package areavoices
+ */
 
-  <!--[if lt IE 7]>
-    <p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to improve your experience.</p>
-  <![endif]-->
+get_header(); ?>
 
-  <header id="header" role="banner">
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
 
-        <div class="super-header">
-          <div class="social-links clearfix">
-            <a href="http://www.facebook.com" class="ir facebook" target="_blank">Facebook</a>
-            <a href="http://www.twitter.com" class="ir twitter" target="_blank">Twitter</a>
-            <a href="http://www.instagram.com" class="ir instagram" target="_blank">Instagram</a>
-          </div><!-- .social-links -->
-        </div><!-- .super-header -->
+		<?php if ( have_posts() ) : ?>
 
-        <div class="site-header">
-          <div class="container">
-            <div class="site-info"
-              <h1>Site Title</h1>
-              <h2>Site Description</h2>
-            </div><!-- .site-info -->
-          </div><!-- .container -->
-        </div><!-- .site-header -->
+			<?php /* Start the Loop */ ?>
+			<?php while ( have_posts() ) : the_post(); ?>
 
-  </header>
+				<?php
 
-  <body class="main">
-    <div class="container">
+					/*
+					 * Include the Post-Format-specific template for the content.
+					 * If you want to override this in a child theme, then include a file
+					 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+					 */
+					get_template_part( 'template-parts/content', get_post_format() );
+				?>
 
-      <div class="banner-ad">
-        <img src="/wp-content/themes/areavoices/images/bannerad.jpg" />
-      </div><!-- .banner-ad -->
+			<?php endwhile; ?>
 
-      <div class="primary span_8_of_12 clearfix">
-        <article>
-          <h1>Heading</h1>
-          <p>Cupcake ipsum dolor sit amet candy canes topping. Chupa chups dessert oat cake cake. Macaroon marzipan unerdwear.com pie toffee dessert gingerbread. Tart candy canes croissant pudding. Halvah liquorice tart tart jelly chocolate cake chocolate unerdwear.com. Chocolate chocolate bar tootsie roll. Unerdwear.com ice cream biscuit biscuit cupcake icing dessert chocolate bar pastry. Bonbon donut cookie unerdwear.com. Gingerbread muffin cupcake dragée soufflé apple pie.</p>
-          <button>Read more...</button>
-        </article>
-        <article>
-          <h1>Heading</h1>
-          <p>Cupcake ipsum dolor sit amet candy canes topping. Chupa chups dessert oat cake cake. Macaroon marzipan unerdwear.com pie toffee dessert gingerbread. Tart candy canes croissant pudding. Halvah liquorice tart tart jelly chocolate cake chocolate unerdwear.com. Chocolate chocolate bar tootsie roll. Unerdwear.com ice cream biscuit biscuit cupcake icing dessert chocolate bar pastry. Bonbon donut cookie unerdwear.com. Gingerbread muffin cupcake dragée soufflé apple pie.</p>
-          <button>Read more...</button>
-        </article>
-        <article>
-          <h1>Heading</h1>
-          <p>Cupcake ipsum dolor sit amet candy canes topping. Chupa chups dessert oat cake cake. Macaroon marzipan unerdwear.com pie toffee dessert gingerbread. Tart candy canes croissant pudding. Halvah liquorice tart tart jelly chocolate cake chocolate unerdwear.com. Chocolate chocolate bar tootsie roll. Unerdwear.com ice cream biscuit biscuit cupcake icing dessert chocolate bar pastry. Bonbon donut cookie unerdwear.com. Gingerbread muffin cupcake dragée soufflé apple pie.</p>
-          <button>Read more...</button>
-        </article>
+			<?php the_posts_navigation(); ?>
 
-        <div class="pagination">
-          Previous | Next
-        </div><!-- .pagination -->
+		<?php else : ?>
 
-      </div><!-- primary -->
+			<?php get_template_part( 'template-parts/content', 'none' ); ?>
 
-      <div class="secondary span_4_of_12">
+		<?php endif; ?>
 
-        <aside class="about">
-          <h2>About Me</h2>
-          <p>Cupcake ipsum dolor sit amet candy canes topping. Chupa chups dessert oat cake cake. Macaroon marzipan unerdwear.com pie toffee dessert gingerbread. Tart candy canes croissant pudding. Halvah liquorice tart tart jelly chocolate cake chocolate unerdwear.com.</p>
-        </aside><!-- .about -->
+		</main><!-- #main -->
+	</div><!-- #primary -->
 
-        <aside class="box-ad">
-          <img src="/wp-content/themes/areavoices/images/ad.jpg" />
-        </aside><!-- .box-ad -->
-
-        <aside class="search-box">
-          <h2>Search</h2>
-        </aside><!-- .search-box -->
-
-        <aside class="popular">
-          <h2>Popular</h2>
-        </aside><!-- .popular -->
-
-      </div><!-- secondary -->
-
-    </div><!-- .container -->
-  </body><!-- .main -->
-
-  <footer>
-  </footer>
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
