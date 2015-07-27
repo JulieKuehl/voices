1c645a439bddb19a1c51bd824df46e0f189035c1
Julie Kuehl <julie@juliekuehl.com>
2015-07-27 12:02:49 -0500

    var dump to customizer.php

--- inc/customizer.php
+++ inc/customizer.php
@@ -11,6 +11,9 @@
  * @param WP_Customize_Manager $wp_customize Theme Customizer object.
  */
 function areavoices_customize_register( $wp_customize ) {
+
+	// var_dump( $wp_customize->settings() );
+
 	$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
 	$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
 	$wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
