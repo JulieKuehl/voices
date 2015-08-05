8ed083a57c00fa888925b105acaaf11e388486db
Julie Kuehl <julie@juliekuehl.com>
2015-08-04 21:01:09 -0500

    eliminated unecessary links in header

--- header.php
+++ header.php
@@ -14,11 +14,7 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="profile" href="http://gmpg.org/xfn/11">
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
-
-<!-- Google Fonts -->
-<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,700' rel='stylesheet' type='text/css'>
 <!-- Stylesheet -->
-<link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/style.css">
 <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/homemade_style.css">
 
 <?php wp_head(); ?>
